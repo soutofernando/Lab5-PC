f4364ad26781804346a89b1e9cb9fb085fa21d81
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2024-09-17 13:06:58 -0300

    lab5

---
+++ go.mod
@@ -0,0 +1,13 @@
+module grpc-filesharing
+
+go 1.23.1
+
+require (
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
+	google.golang.org/grpc v1.66.2 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
---
+++ src/server/server.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+
+	pb "../proto/fileSearch"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/peer"
+)
+
+type server struct {
+	pb.UnimplementedFileSearchServer
+	mu       sync.Mutex
+	fileMap  map[string][]string
+	reqChan  chan request
+	respChan chan response
+}
+
+type request struct {
+	hash   string
+	client string
+}
+
+type response struct {
+	hash string
+	ips  []string
+}
+
+func newServer() *server {
+	return &server{
+		fileMap:  make(map[string][]string),
+		reqChan:  make(chan request),
+		respChan: make(chan response),
+	}
+}
+
+func (s *server) SendFileHashes(ctx context.Context, req *pb.FileHashes) (*pb.Response, error) {
+	clientIP, _ := getClientIP(ctx)
+	
+	for _, hash := range req.Hashes {
+		s.mu.Lock()
+		s.fileMap[hash] = append(s.fileMap[hash], clientIP)
+		s.mu.Unlock()
+	}
+	
+	return &pb.Response{Message: "Hashes received"}, nil
+}
+
+func (s *server) GetMachinesWithFile(ctx context.Context, req *pb.FileHash) (*pb.FileLocations, error) {
+	s.mu.Lock()
+	ips := s.fileMap[req.Hash]
+	s.mu.Unlock()
+	return &pb.FileLocations{Ips: ips}, nil
+}
+
+func getClientIP(ctx context.Context) (string, error) {
+	pr, ok := peer.FromContext(ctx)
+	if !ok {
+		return "", fmt.Errorf("erro ao obter IP do cliente")
+	}
+	return pr.Addr.String(), nil
+}
+
+func (s *server) run() {
+	for {
+		select {
+		case req := <-s.reqChan:
+			s.mu.Lock()
+			ips := s.fileMap[req.hash]
+			s.mu.Unlock()
+			s.respChan <- response{hash: req.hash, ips: ips}
+		}
+	}
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	s := newServer()
+	go s.run()
+
+	pb.RegisterFileSearchServer(grpcServer, s)
+	log.Printf("Servidor rodando na porta :50051")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
